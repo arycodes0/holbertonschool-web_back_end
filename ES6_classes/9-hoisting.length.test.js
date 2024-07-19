9b3b4a7f58f1ac1714b869717d56076736b49e07
arycodes0 <alvarezaryam@gmail.com>
2024-07-19 02:50:15 -0400

    Add test for task 9, tests lenght of listofStudents

--- ES6_classes/9-hoisting.length.test.js
+++ ES6_classes/9-hoisting.length.test.js
@@ -1,6 +1,9 @@
 // Testing lenght of listofStudents 9-hoisting.length.test.js
 import { listOfStudents } from './9-hoisting';
 
-test("listOfStudents has the correct length", () => {
-  expect(listOfStudents.length).toBe(5);
+describe('testing listOfStudents', () => {
+  it('listOfStudents has the correct length', () => {
+    expect.assertions(1);
+    expect(listOfStudents).toHaveLength(5);
+  });
 });
