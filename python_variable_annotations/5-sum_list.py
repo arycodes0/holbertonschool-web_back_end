08a4aa57b3c6635290dcea6e88e6775a978577ed
arycodes0 <alvarezaryam@gmail.com>
2024-08-16 16:05:07 -0400

    refactor: Calculate and return the sum of a list of floats

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """
+    Calculate and return the sum of a list of floats.
+
+    Args:
+        input_list (List[float]): A list of float numbers to be summed.
+
+    Returns:
+        float: The sum of all floats in the input_list.
+    """
+    return sum(input_list)
