abae4f7ac6f409df97b7f121f768fb09d04c3e9c
arycodes0 <alvarezaryam@gmail.com>
2024-08-16 14:47:14 -0400

    refactor: Returns a concatenated string

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""This module contains a function that concatenate two strings."""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Returns the concatenation of str1 and str2."""
+    return str1 + str2
