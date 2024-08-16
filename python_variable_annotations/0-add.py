3e1cceefd470d52c5451a3aea3e168d35ba1663f
arycodes0 <alvarezaryam@gmail.com>
2024-08-16 14:36:03 -0400

    refactor: The sum of two floats

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""This module contains a function that returns the sum of two floats."""
+
+
+def add(a: float, b: float) -> float:
+    """Returns the sum of a and b."""
+    return a + b
