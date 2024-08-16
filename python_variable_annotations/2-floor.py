5c5de6423e600918960a4339e73a483572b15ad6
arycodes0 <alvarezaryam@gmail.com>
2024-08-16 15:12:05 -0400

    refactor: Returns the floor of the float.

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""This module contains a function that takes a float as arg
+and returns the floor of the float."""
+
+
+import math
+
+def floor(n: float) -> int:
+    """Returns the floor of the float n."""
+    return math.floor(n)
