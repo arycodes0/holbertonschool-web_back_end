3a38cfe1d5c100e11bcc043e1fa51e9d8a873e80
arycodes0 <alvarezaryam@gmail.com>
2024-08-16 15:32:30 -0400

    refactor: Returns the string representation of the float n

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""This module contains a function that takes a float as arg
+and returns the str representation of the float."""
+
+
+def to_str(n: float) -> str:
+    """Returns the string representation of the float n."""
+    return str(n)
