7821991121e8ae7faeb711bb62ea6fa142a08929
arycodes0 <alvarezaryam@gmail.com>
2024-08-16 15:52:15 -0400

    refactor: Defines and annotates several variables

---
+++ python_variable_annotations/4-define_variables.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""This module defines and annotates several variables."""
+
+a: int = 1
+"""a is an integer with a value of 1."""
+
+pi: float = 3.14
+"""pi is a float with a value of 3.14."""
+
+i_understand_annotations: bool = True
+"""i_understand_annotations is a boolean with a value of True."""
+
+school: str = "Holberton"
+"""school is a string with a value of 'Holberton'."""
