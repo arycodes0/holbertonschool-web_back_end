03ff0f757a47e12b5f08b79a93e94ba98d6a5346
arycodes0 <alvarezaryam@gmail.com>
2024-08-29 13:35:13 -0400

    style: Eliminating whitespaces

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -2,9 +2,9 @@
 
 def index_range(page: int, page_size: int) -> tuple:
     """
-    Calculate the start and end index for a pagination system. 
-    
-    Args: 
+    Calculate the start and end index for a pagination system.
+
+    Args:
         page (int): The current page number (1-indexed).
         page_size (int): The number of items per page.
 
@@ -16,4 +16,3 @@ def index_range(page: int, page_size: int) -> tuple:
     start_index = (page - 1) * page_size
     end_index = start_index + page_size
     return start_index, end_index
-    
