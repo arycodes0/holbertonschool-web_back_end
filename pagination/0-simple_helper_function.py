956a3352914ccd220214bd11fc28adf412f603e0
arycodes0 <alvarezaryam@gmail.com>
2024-08-29 13:44:18 -0400

    doc: Adding documentation

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+""" Simple pagination module. """
+
 def index_range(page: int, page_size: int) -> tuple:
     """
     Calculate the start and end index for a pagination system.
